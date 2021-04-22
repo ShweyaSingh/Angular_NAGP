70ae0c6c91adc9a7be18f6bafc8973bbcb1a1a5a
shweta03 <shweta03@nagarro.com>
2021-04-22 14:51:03 +0530

    add to cart option on list page for authenticated user

--- src/app/product/list/product-list.component.ts
+++ src/app/product/list/product-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { AppConstants } from 'src/app/shared/constant/app.constant';
 import { Product } from 'src/app/shared/models/product';
-import { ProductCategory } from 'src/app/shared/models/product-category';
+import { CartService } from 'src/app/shared/services/cart.service';
 import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
@@ -11,7 +11,11 @@ import { ProductService } from 'src/app/shared/services/product.service';
 export class ProductListComponent implements OnInit {
   public products: Product[];
 
-  constructor(private router: Router, private productService: ProductService) {}
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
 
   public ngOnInit(): void {
     this.productService.getAllProducts().subscribe((response) => {
@@ -19,7 +23,34 @@ export class ProductListComponent implements OnInit {
     });
   }
 
+  public get isLoggedIn(): boolean {
+    return (
+      !!localStorage.getItem('TOKEN') &&
+      localStorage.getItem('TOKEN') === AppConstants.authToken &&
+      !!localStorage.getItem('EMAIL')
+    );
+  }
+
   public viewDetailPage(id: number): void {
     this.router.navigate(['products/product/' + id]);
+  }
+
+  public addToCart(product: Product): void {
+    if (this.isLoggedIn) {
+      const email = localStorage.getItem('EMAIL');
+      if (!!email) {
+        this.cartService.addProduct(product, email).subscribe((response) => {
+          if (response.success) {
+            this.router.navigate(['user/cart']);
+          } else {
+            this.router.navigate(['user/login']);
+          }
+        });
+      } else {
+        this.router.navigate(['user/login']);
+      }
+    } else {
+      this.router.navigate(['user/login']);
+    }
   }
 }
